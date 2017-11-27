c1f20ae86c87587b76d5ec85b67164aff71773c1
KataBucsai <kata.kovacs@gmail.com>
2017-11-27 17:40:53 +0100

    implement treaadpool task

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback waitCallback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(waitCallback, "Hello");
+            ThreadPool.QueueUserWorkItem(waitCallback, "Hi");
+            ThreadPool.QueueUserWorkItem(waitCallback, "Heya");
+            ThreadPool.QueueUserWorkItem(waitCallback, "Goodbye");
+            Console.ReadKey();
+        }
+
+        static void ShowMyText(object state)
+        {
+            string text = state.ToString();
+            Console.WriteLine(text + " " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
