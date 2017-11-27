48f89b6781bc5b2dcbc7d0be11e31c9e85f91ef4
KataBucsai <kata.kovacs@gmail.com>
2017-11-27 16:48:57 +0100

    implement multiple threads task

--- SimpleThreadingDemo/Program.cs
+++ SimpleThreadingDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimpleThreadingDemo
 {
@@ -10,6 +11,26 @@ namespace SimpleThreadingDemo
     {
         static void Main(string[] args)
         {
+            ThreadStart start = new ThreadStart(Counting);
+            Thread thread1 = new Thread(start);
+            Thread thread2 = new Thread(start);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.ReadLine();
+        }
+
+        static void Counting()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i + " " + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
         }
     }
 }
